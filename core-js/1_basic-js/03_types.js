ed72d4ca5c9f9e6c9d9ffc47632d9011e80a3cea
Jkey-u <dum6894@gmail.com>
2022-03-01 22:43:58 +0900

    자료형 (Types)

---
+++ core-js/1_basic-js/03_types.js
@@ -0,0 +1,8 @@
+// [자료형] https://ko.javascript.info/types
+
+// Q) 결과 예측
+let name = "Ilya";
+
+console.log(`hello ${1}`); // hello 1
+console.log(`hello ${"name"}`); // hello name
+console.log(`hello ${name}`); // hello Ilya
