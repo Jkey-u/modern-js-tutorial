1640587bf9c8ffc7e3fb8bab08136149a387c86d
Jkey-u <dum6894@gmail.com>
2022-03-01 22:43:10 +0900

    Initial commit

---
+++ core-js/1_basic-js/01_helloWorld.js
@@ -0,0 +1,8 @@
+// https://ko.javascript.info/hello-world
+
+/*
+    스크립트를 별도의 파일에 작성하면, 브라우저가 스크립트를 다운받아 캐시(cache)에 저장하기 때문에, 성능상의 이점이 있다.
+    트래픽 절약, 웹 페이지의 실제 속도 향상
+*/
+
+console.log("I'm JavaScript!");
